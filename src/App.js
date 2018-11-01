e30516f1234f85c7712093d32b9a68558b814090
Vadim Cpp <vadim.cpp@gmail.com>
2018-11-01 20:42:48 +0300

    Add appheader component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class AppHeader extends Component {
   render() {
     return (
+      <header className="App-header">
+        <h1 className="App-header-title"> Москва - Тюмень </h1>
+        <p className="App-header-subtitle"> Шаг 1 из 5. Выберите рейс «Туда» </p>
+      </header>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (      
       <div className="App">
+        <AppHeader />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>

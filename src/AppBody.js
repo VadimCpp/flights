2a4ad35ae29a9fa28846823823cdf2d88ee551a8
Vadim Cpp <vadim.cpp@gmail.com>
2018-11-02 14:23:42 +0300

    Add Calendar and Details blank components

--- src/AppBody.js
+++ src/AppBody.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Calendar from './Calendar.js'
+import Details from './Details.js'
 
 class AppBody extends Component {
   render() {
     return (
       <div className="App-body">
-        TODO:        
+        <Calendar />
+		<Details />     
       </div>
     )
   }

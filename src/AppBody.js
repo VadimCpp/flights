5ffbd4edd6d949e1ad1a6c874b5bdefdf86e5de7
Vadim Cpp <vadim.cpp@gmail.com>
2018-11-02 12:03:58 +0300

    Add AppBody component

---
+++ src/AppBody.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AppBody extends Component {
+  render() {
+    return (
+      <div className="App-body">
+        TODO:        
+      </div>
+    )
+  }
+}
+
+
+export default AppBody;

22ec7918ee61ba0c315a4e81092e45412c45a850
Vadim Cpp <vadim.cpp@gmail.com>
2018-11-03 17:40:26 +0300

    Update header style

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlane } from '@fortawesome/free-solid-svg-icons'
 import './AppHeader.css';
 
 class AppHeader extends Component {
   render() {
     return (
       <header className="App-header">
-        <h1 className="App-header-title"> Москва - Тюмень </h1>
-        <p className="App-header-subtitle"> Шаг 1 из 5. Выберите рейс «Туда» </p>
+        <h1 className="App-header_title"> 
+        	<span className="App-header_title__departure">Москва</span> 
+        	<FontAwesomeIcon icon={faPlane} /> 
+        	<span className="App-header_title__arrival">Тюмень</span>
+    	</h1>
+        <p className="App-header_subtitle"> Шаг 1 из 5. Выберите рейс «Туда» </p>
       </header>
     )
   }

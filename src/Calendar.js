bca0619ed07f2784f809df001f53da82b2096181
Vadim Cpp <vadim.cpp@gmail.com>
2018-11-03 16:02:35 +0300

    Implement simple horisontal movement

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,24 +1,59 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
+import './Calendar.css';
+
+function SliderBody(props) {
+  return (
+    <div className="slider-body" style={props.styles}>
+      Slider body
+    </div>
+  );
+}
 
 class Calendar extends Component {
   constructor(props) {
     super(props);
-    this.state = {month: 'April'};
+    this.state = {
+      month: 'April',
+      position: 0,
+      step: 100,
+    };
   }
   
-  render() {
+  swipeLeft() {
+    const position = this.state.position - this.state.step;
+    console.log('swipeLeft to ' + position);
+    this.setState({
+      position: position,
+    });
+  }
+
+  swipeRight() {
+    const position = this.state.position + this.state.step;
+    console.log('swipeRight to ' + position);
+    this.setState({
+      position: position,      
+    });
+  }
+
+  render() {    
     return (
       <div className="calendar">
-        <div class="calendar-header">
+        <div className="calendar-header">
           <FontAwesomeIcon icon={faCalendarAlt} />
-          <span class="month">          
+          <span className="month">          
             {this.state.month}
           </span> 
         </div>
-        <div class="calendar-slider">
-          TODO: calendar-slider
+        <button onClick={() => (this.swipeLeft())}>
+          Left
+        </button>
+        <button onClick={() => (this.swipeRight())}>
+          Right
+        </button>
+        <div className="calendar-slider">          
+          <SliderBody styles={{left: this.state.position}} />
         </div>
       </div>
     )

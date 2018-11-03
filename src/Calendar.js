a0d21c4abec06bdd784828aa3bc9814438e7eb2b
Vadim Cpp <vadim.cpp@gmail.com>
2018-11-03 19:17:33 +0300

    Add moment js

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
+import 'moment/locale/ru';
 import Slider from './slider/Slider.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
@@ -7,21 +9,26 @@ import './Calendar.css';
 class Calendar extends Component {
   constructor(props) {
     super(props);
+    
     this.state = {
-      month: 'April',
       position: 0,
       step: 100,
+      today: new Date(),
     };
   }
 
   render() {    
-    return (
+    return (    
       <div className="calendar">
         <div className="calendar-header">
           <FontAwesomeIcon icon={faCalendarAlt} />
-          <span className="calendar-header_month">          
-            {this.state.month}
-          </span> 
+          <Moment 
+            className="calendar-header_month"
+            locale="ru"
+            format="MMMM"
+          >
+            {this.state.today}
+          </Moment>
         </div>
         <div className="calendar-slider">          
           <Slider />

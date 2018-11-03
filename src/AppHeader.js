3b608fc67b433768b9db3e208d070555c8fb6f5b
Vadim Cpp <vadim.cpp@gmail.com>
2018-11-03 17:48:51 +0300

    Add back button to header

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlane } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import './AppHeader.css';
 
 class AppHeader extends Component {
+  moveBack() {
+    alert('Not implemented');
+  }
+
   render() {
     return (
       <header className="App-header">
         <h1 className="App-header_title"> 
-        	<span className="App-header_title__departure">Москва</span> 
-        	<FontAwesomeIcon icon={faPlane} /> 
-        	<span className="App-header_title__arrival">Тюмень</span>
+          <span className="App-header_title__departure">Москва</span> 
+          <FontAwesomeIcon icon={faPlane} /> 
+          <span className="App-header_title__arrival">Тюмень</span>
     	</h1>
         <p className="App-header_subtitle"> Шаг 1 из 5. Выберите рейс «Туда» </p>
+        <button 
+          onClick={() => { this.moveBack()}}
+          className="App-header_back"
+        > 
+          <FontAwesomeIcon icon={faChevronLeft} />
+    	</button>
       </header>
     )
   }

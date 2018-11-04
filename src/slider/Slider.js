46c43ea6654c9e51cc1793782a026abf30adb39b
Vadim Cpp <vadim.cpp@gmail.com>
2018-11-04 22:00:44 +0300

    Display current date

--- src/slider/Slider.js
+++ src/slider/Slider.js
@@ -5,23 +5,40 @@ import './Slider.css';
 
 class Slider extends Component {
   render() {    
+    const delta = [-2, -1, 0, 1, 2];
+    const dates = delta.map((delta) => {
+      let day = new Date(this.props.currentDate.getTime());
+      day.setDate(this.props.currentDate.getDate() + delta);      
+      return day;
+    });
+    const listItems = dates.map((date) =>
+      <li key={date.toString()} className='slider-item'>
+        <p>
+          <Moment locale="ru" format="dd">
+            {date}
+          </Moment>
+        </p>
+        <p>
+          {date.getTime() === this.props.currentDate.getTime() ? (
+              <Moment locale="ru" format="D" className="selected-date">
+                {date}
+              </Moment>
+            ) : (
+              <Moment locale="ru" format="D">
+                {date}
+              </Moment>
+            )
+          }
+        </p>
+        <p>
+          5 185+
+        </p>      
+      </li>
+    );
+
     return (
       <ul className="slider">
-        <li>
-          <p>
-            <Moment locale="ru" format="dd">
-              {this.props.currentDate}
-            </Moment>
-          </p>
-          <p>
-            <Moment locale="ru" format="D">
-              {this.props.currentDate}
-            </Moment>
-          </p>
-          <p>
-            TODO: price
-          </p>
-      	</li>
+        {listItems}
       </ul>
     )
   }  

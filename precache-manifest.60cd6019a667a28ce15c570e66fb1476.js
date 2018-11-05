5100ad327e505b2dc5475fbf1dc18799d6a16424
Vadim Cpp <vadim.cpp@gmail.com>
2018-11-06 00:41:24 +0300

    Updates

---
+++ precache-manifest.60cd6019a667a28ce15c570e66fb1476.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4db4f9416ff1b256964a",
+    "url": "/VadimCpp/flights/static/js/runtime~main.4db4f941.js"
+  },
+  {
+    "revision": "ca94b276c7628d8c388c",
+    "url": "/VadimCpp/flights/static/js/main.ca94b276.chunk.js"
+  },
+  {
+    "revision": "57b0c27d496dd33af789",
+    "url": "/VadimCpp/flights/static/js/1.57b0c27d.chunk.js"
+  },
+  {
+    "revision": "ca94b276c7628d8c388c",
+    "url": "/VadimCpp/flights/static/css/main.951ab7b4.chunk.css"
+  },
+  {
+    "revision": "16ced3175e7cb6abbee14e1fd256a6f4",
+    "url": "/VadimCpp/flights/index.html"
+  }
+];

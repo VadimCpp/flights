ba22e70099f402ae5f0445596073555f5ce1fee7
Vadim Cpp <vadim.cpp@gmail.com>
2018-11-08 11:27:46 +0300

    Add simple pose animation

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+import posed from 'react-pose';
 import Moment from 'react-moment';
 import 'moment/locale/ru';
-import Slider from './slider/Slider.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
+import Slider from './slider/Slider.js';
 import './Calendar.css';
 
+const Box = posed.div({
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 }
+});    
+
 class Calendar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isVisible: true,
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ isVisible: !this.state.isVisible });
+    }, 1000);
+  }
+
   onDateClick(date) {
     this.props.onDateChange(date);    
   }
 
-  render() {    
+  render() {
     return (    
       <div className="calendar">
         <div className="calendar-header">
@@ -30,6 +50,7 @@ class Calendar extends Component {
             onDateClick={(date) => {this.onDateClick(date)}}
           />
         </div>
+        <Box className="box" pose={this.state.isVisible ? 'visible' : 'hidden'}/>
       </div>
     )
   }

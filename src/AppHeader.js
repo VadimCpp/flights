8f31c0d3f7bb9fe4b4b00612ec706677e6b43f0e
Vadim Cpp <vadim.cpp@gmail.com>
2018-11-11 01:27:10 +0300

    Update title

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlane } from '@fortawesome/free-solid-svg-icons'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import './AppHeader.css';
 
@@ -13,11 +12,11 @@ class AppHeader extends Component {
     return (
       <header className="App-header">
         <h1 className="App-header_title"> 
-          <span className="App-header_title__departure">Москва</span> 
-          <FontAwesomeIcon icon={faPlane} /> 
-          <span className="App-header_title__arrival">Тюмень</span>
-    	  </h1>
-        <p className="App-header_subtitle"> Шаг 1 из 5. Выберите рейс «Туда» </p>
+          Прогноз погоды
+        </h1>
+        <p className="App-header_subtitle"> 
+          с разбивкой по 3 часа, все как надо
+        </p>
         <button 
           onClick={() => { this.moveBack()}}
           className="App-header_back"
